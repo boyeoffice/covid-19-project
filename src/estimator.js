04f2687819e9f6c1ab2bd1282238423ee69dd56c
Olakunle Boye <kunlexzy@gmail.com>
2020-04-12 21:18:39 +0100

    testing challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,33 @@
+const estimateCurrentlyInfected = ({ data, impact, severeImpact }) => {
+  impact.currentlyInfected = Math.trunc(data.reportedCases * 10);
+  severeImpact.currentlyInfected = Math.trunc(data.reportedCases * 50);
+  return { impact, severeImpact };
+};
+
+const estimateProjectedInfections = ({ data, impact, severeImpact }) => {
+  const periodEstimator = (period, time) => {
+    switch (period) {
+      case 'months':
+        return time * 30;
+      case 'weeks':
+        return time * 7;
+      default:
+        return time;
+    }
+  };
+  impact.infectionsByRequestedTime = Math.trunc(impact.currentlyInfected
+    * (2 ** (Math.trunc(periodEstimator(data.periodType, data.timeToElapse) / 3))));
+  severeImpact.infectionsByRequestedTime = Math.trunc(severeImpact.currentlyInfected
+  * (2 ** (Math.trunc(periodEstimator(data.periodType, data.timeToElapse) / 3))));
+  return { impact, severeImpact };
+};
+
 const covid19ImpactEstimator = (data) => {
-  const estimator = chain(estimateCurrentlyInfected);
+  const estimator = ({ impact, severeImpact }) => {
+    estimateCurrentlyInfected({ data, impact, severeImpact });
+    estimateProjectedInfections({ data, impact, severeImpact });
+    return { data, impact, severeImpact };
+  };
   return estimator({
     data,
     impact: {},
--- src/ts.js
+++
@@ -1,6 +0,0 @@
-const test1 = () => {
-  const impact = 1;
-  const severeImpact = 2;
-  return { impact, severeImpact };
-};
-console.log(test1());
