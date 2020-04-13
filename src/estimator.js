8a2754dcd5e124b281900911a7399b21fe5f9fdd
Olakunle Boye <kunlexzy@gmail.com>
2020-04-13 18:27:23 +0100

    test challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -39,6 +39,30 @@ const estimateBedSpaceAvailability = ({ data, impact, severeImpact }) => {
   return { impact, severeImpact };
 };
 
+const estimateCasesForICU = ({ impact, severeImpact }) => {
+  impact.casesForICUByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.05);
+  severeImpact.casesForICUByRequestedTime = Math.trunc(
+    severeImpact.infectionsByRequestedTime * 0.05
+  );
+  return { impact, severeImpact };
+};
+const estimateCasesForVentilators = ({ impact, severeImpact }) => {
+  impact.casesForVentilatorsByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.02);
+  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(
+    severeImpact.infectionsByRequestedTime * 0.02
+  );
+  return { impact, severeImpact };
+};
+const estimateDollarInFlight = ({ data, impact, severeImpact }) => {
+  impact.dollarsInFlight = Math.trunc((impact.infectionsByRequestedTime
+      * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD)
+      / Math.trunc(periodEstimator(data.periodType, data.timeToElapse)));
+  severeImpact.dollarsInFlight = Math.trunc((severeImpact.infectionsByRequestedTime
+    * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD)
+    / Math.trunc(periodEstimator(data.periodType, data.timeToElapse)));
+  return { impact, severeImpact };
+};
+
 const covid19ImpactEstimator = (data) => {
   const estimator = ({ impact, severeImpact }) => {
     // challenge 1
@@ -47,6 +71,10 @@ const covid19ImpactEstimator = (data) => {
     // challenge 2
     estimateSevereCases({ impact, severeImpact });
     estimateBedSpaceAvailability({ data, impact, severeImpact });
+    // challenge 3
+    estimateCasesForICU({ impact, severeImpact });
+    estimateCasesForVentilators({ impact, severeImpact });
+    estimateDollarInFlight({ data, impact, severeImpact });
     return { data, impact, severeImpact };
   };
   return estimator({
